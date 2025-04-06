66ff919a3a6089fad411cb8d28bf95b206aafd54
Mrityunjay Singh <mrityunjaysing@gmail.com>
2025-04-06 21:18:55 +0530

    Add day1_practice.py

---
+++ day1_practice.py
@@ -0,0 +1,9 @@
+print("Name: Mrityunjay Singh")
+print("City: Delhi")
+print("Age: ", 49)
+age = input("Enter age: ")
+future = int(age) +10
+print("Age in 10 years:", future)
+number1 = input("Enter number1: ")
+number2 = input("Enter number2: ")
+print("No.1 x No.2", int(number1) * int(number2))
