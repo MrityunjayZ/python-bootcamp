4d4c72199580e7087675a0a102af4114a9e4e024
Mrityunjay Singh <mrityunjaysing@gmail.com>
2025-04-08 11:50:18 +0530

    Add day3_notes.py

---
+++ day3_notes.py
@@ -0,0 +1,11 @@
+Day 3 Kickoff: Python Bootcamp
+Step 1: Quick Recap
+Here are 5 warm-up questions. Answer them directly here or in your day3_notes.py file.
+
+1. What’s the difference between = and == in Python?
+A. = is an assignment operator. you can assign a vallue to any thing usin =, for eg. a=10 in this a variable is being assigned the value 10 
+   == oprerator compares two values if they are equal. returns Tru if equal or False if not. eg 5==10 will return False and 10==10 will return True
+2. How do you take input from the user and convert it to an integer?
+a = input("Enter a value with decimal places: ")
+print("integer value is: ",int(a))
+What’s the output of this code?
