61453e8c223d6e5228020f65329c1ed591380b7c
Mrityunjay Singh <mrityunjaysing@gmail.com>
2025-04-08 11:49:02 +0530

    Add day3_warmup.py

---
+++ day3_warmup.py
@@ -0,0 +1,6 @@
+user_studyhrs = int
+user_studymin = int
+user_studyhrs = int(input("How many hrs did you study today: "))
+user_studymin = user_studyhrs * 60
+print(f"You studied {user_studyhrs} hours today, that's {user_studymin} minutes! Keep it up!")
+                         
