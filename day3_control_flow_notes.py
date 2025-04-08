404330b7ed7d8b5ca58294f41e8eb41ed62e5312
Mrityunjay Singh <mrityunjaysing@gmail.com>
2025-04-08 13:22:43 +0530

    Add day3_control_flow_notes.py

--- day3_control_flow_notes.py
+++ day3_control_flow_notes.py
@@ -18,16 +18,31 @@ print(10 == 10) #True
 print(10 != 5) #True
 Notes: the above code gives true or false value for the numbers compared, in this case 10 and 5, the last one is different as it checks if the numbers are unequal so True
 
-#  3. Logical Operators
+#  3a. Logical Operators
 
 user_age = int(input("Enter your age: "))
 age = user_age
-has_license = True
-if age >= 18 and has_license:
+has_licence = True
+if age >= 18 and has_licence:
     print("You can drive")
 else:
     print("You cannot drive")
 Notes: above code asks for the user to enter their age and checks parameters if the person is 18 years or above with driving licence then the he\she can drive else if the age is below 18 thenthey cannot drive
+
+#  3b. Logical Operators
+
+user_age = int(input("Enter your age: "))
+user_licence = input("Do you have a licence? (Y/N): ").strip().upper()
+
+if user_age >= 18 and user_licence == "Y":
+    print("You're of age and have a license. You can drive!")
+elif user_age >= 18 and user_licence == "N":
+    print("You're of age but need to get a license.")
+else:
+    print("You're not old enough to drive yet.")
+
+
+Notes:more refined way with accepting lower case and converting it to upper case Y and N 
 
 # 4. Nested Ifs & Multiple Conditions
 
@@ -41,4 +56,4 @@ elif marks >= 60:
 else:
     print("Grade: D or Fail")
 Notes: above code checks what grade the user has secured, it compares with the rules equal or above 90 is A grade, equal or above 70 is B grade, equal or above 60 is C grade and anything below 60 is D grade or Fail 
-  
+
