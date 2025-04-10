18c39387645a9376eb621be3533fd6b335b2d98c
Mrityunjay Singh <mrityunjaysing@gmail.com>
2025-04-10 12:47:55 +0530

    Add revision_day2.py

---
+++ revision_day2.py
@@ -0,0 +1,9 @@
+user_num = int(input("Enter a number: ")) #Takes a number from the user
+
+print(user_num ** 2)     # Prints its square, cube, and square root
+print(user_num ** 3)
+print(user_num ** 0.5)
+if user_num % 2 == 0:  # Prints whether the number is even or odd
+    print("number entered is even") # Even 
+else:
+    print("number entered is odd") # Odd 
